7b9fdf8d227686d21b731eab56059b4950886be0
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-12 05:38:58 +0900

    Add scala/string.scala

---
+++ scala/string.scala
@@ -0,0 +1,47 @@
+val something: String = "something"
+
+// [v2.11.7]
+// 9
+// 9
+something.length
+something.size
+
+// [v2.11.7]
+// prefix-something
+// something-suffix
+"prefix-" + something
+something.concat("-suffix")
+
+// [v2.11.7]
+// 4
+// 4
+something.indexOf("t")
+something.lastIndexOf("t")
+
+// [v2.11.7]
+// true
+something.matches("some.*")
+
+// [v2.11.7]
+// SOMEthing
+// someTHINGsomeTHING
+something.replaceFirst("some", "SOME")
+(something * 2).replaceAll("thing", "THING")
+
+// [v2.11.7]
+// Array(some, hing)
+something.split("t")
+
+// [v2.11.7]
+// thing
+something.substring(4)
+
+// [v2.11.7]
+// SOMETHING
+// something
+something.toUpperCase
+something.toUpperCase.toLowerCase
+
+// [v2.11.7]
+// something
+"  something   ".trim
