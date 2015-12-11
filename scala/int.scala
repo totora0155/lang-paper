b29e046e0c977e2a8ec81c7d548ef3345a37f714
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-12 06:58:33 +0900

    Add scala/int.scala

---
+++ scala/int.scala
@@ -0,0 +1,27 @@
+val something: Int = 13
+
+// [v2.11.7]
+// d
+something.toHexString
+
+// [v2.11.7]
+// 20
+// 10
+// 130
+// 1
+// 3
+something + 7
+something - 3
+something * 10
+something / 10
+something % 10
+
+// [v2.11.7]
+// 13
+-something.abs
+
+// [v2.11.7]
+// Range(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+// Range(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+1 to something
+1 until something
