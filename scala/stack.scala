774fd87ed4757e233be27dd7a3ee31c2d38237d5
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-15 12:05:39 +0900

    Update scala/stack.scala

--- scala/stack.scala
+++ scala/stack.scala
@@ -1,12 +1,20 @@
 // [v2.11.7]
+// 1
+// Stack()
+val ms = scala.collection.immutable.Stack.empty
+val hasOne = ms.push(1)
+hasOne.top
+hasOne.pop
+
+// [v2.11.7]
 // Stack(1)
 // Stack(2, 1)
 // 2
 // 2
 // 1
-val stack = new scala.collection.mutable.Stack[Int]
-stack.push(1)
-stack.push(2)
-stack.top
-stack.pop
-stack.top
+val ims = new scala.collection.mutable.Stack[Int]
+ims.push(1)
+ims.push(2)
+ims.top
+ims.pop
+ims.top
