4924e1425ae6479605b1d2aff3854b5c7386fa95
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-15 13:04:08 +0900

    Rename vector -> view & Update

--- scala/view.scala
+++ scala/view.scala
@@ -9,3 +9,13 @@ val vvmm: scala.collection.SeqView[Int,Seq[_]] = vvm map (_ * 2)
 // scala.collection.SeqView[Int,Seq[_]] = SeqViewMMR(...)
 val vvmmr: scala.collection.SeqView[Int,Seq[_]] = vvmm.reverse
 vvmmr.force
+
+// [v2.11.7]
+// Array(0, 1, -2, -3, -4, -5, 6, 7, 8, 9)
+val arr: Array[Int] = (0 to 9).toArray
+val vs: scala.collection.mutable.IndexedSeqView[
+Int,Array[Int]] = arr.view.slice(2, 6)
+def negate(xs: collection.mutable.Seq[Int]) =
+  for (i <- 0 until xs.length) xs(i) = -xs(i)
+negate(vs)
+arr
