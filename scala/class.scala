29fc114c0d290d2f6c7e27a0c67615c340a0c257
nju33 <jun.sasaki@mvrck.co.jp>
2015-12-16 18:48:38 +0900

    Add scala/class.scala

---
+++ scala/class.scala
@@ -0,0 +1,28 @@
+class Animal(name: String) {
+  def get() = name
+}
+
+// [v2.11.7]
+// dog
+val dog: Animal = new Animal("dog")
+dog.get
+
+case class People(name: String, age: Int)
+
+// [v2.11.7]
+// People(john, 23)
+val people: People = People("john", 23)
+
+// [v2.11.7]
+// name: john, age: 23
+people match {
+  case People(name, age) => s"name: ${name}, age: ${age}"
+  case _ => ""
+}
+
+// [v2.11.7]
+// john
+// 23
+val People(name, age) = people
+// name
+// age
